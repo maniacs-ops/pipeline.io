6314cdfb7fb49f9cbaba010995704eb8e33a2774
cfregly <chris@fregly.com>
2016-11-08 00:14:38 +0000

    fixed sbt-launch, removed mnist

--- tensorflow/setup-tensorflow-serving.sh
+++ tensorflow/setup-tensorflow-serving.sh
@@ -12,8 +12,8 @@ bazel build //tensorflow_serving/example:inception_inference
 bazel build //tensorflow_serving/example:inception_client
 bazel build //tensorflow_serving/example:inception_export
 
-echo '...Build the MNIST Image Classifier Components (10-15 mins)...'
-cd $TENSORFLOW_SERVING_HOME
-bazel build //tensorflow_serving/example:mnist_inference_2
-bazel build //tensorflow_serving/example:mnist_client
-bazel build //tensorflow_serving/example:mnist_export
+#echo '...Build the MNIST Image Classifier Components (10-15 mins)...'
+#cd $TENSORFLOW_SERVING_HOME
+#bazel build //tensorflow_serving/example:mnist_inference_2
+#bazel build //tensorflow_serving/example:mnist_client
+#bazel build //tensorflow_serving/example:mnist_export
