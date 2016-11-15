1c0f0524b123be64453380f86ac3ddebd74bd3f1
Chris Fregly <chris@fregly.com>
2016-11-15 17:32:56 +0000

    Update datasticks-github-push.sh

--- bin/datasticks-github-push.sh
+++ bin/datasticks-github-push.sh
@@ -1,5 +1,4 @@
 #!/bin/sh                                                                               
-<<<<<<< HEAD
                                                                                         
 eval $(ssh-agent -s)                                                                    
                                                                                         
@@ -14,9 +13,3 @@ git pull
 git add pmml/data/census/census.pmml                                                    
 git commit -m "updated pmml"                                                            
 git push
-=======
-cd /root/prediction.ml                                                                              
-git add census.pmml
-git commit -m "updated pmml"
-git push 
->>>>>>> e9e331f628b6c69cd26a75f120226d705c685f99
