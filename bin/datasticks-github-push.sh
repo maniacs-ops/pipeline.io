e9e331f628b6c69cd26a75f120226d705c685f99
cfregly <chris@fregly.com>
2016-11-15 16:31:52 +0000

    added push script for github

---
+++ bin/datasticks-github-push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh                                                                               
+cd /root/prediction.ml                                                                              
+git add census.pmml
+git commit -m "updated pmml"
+git push 
