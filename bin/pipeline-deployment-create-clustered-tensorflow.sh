7d2c2a1683d42debe68208df0327d5bd082e2f47
Chris Fregly <chris@fregly.com>
2016-11-22 07:34:29 +0100

    Create pipeline-deployment-create-clustered-tensorflow.sh

---
+++ bin/pipeline-deployment-create-clustered-tensorflow.sh
@@ -0,0 +1,15 @@
+echo '...PS0...'
+kubectl create -f ../clustered.ml/tensorflow-ps0-rc.yaml
+kubectl create -f ../clustered.ml/tensorflow-ps0-svc.yaml
+
+echo '...PS1...'
+kubectl create -f ../clustered.ml/tensorflow-ps1-rc.yaml
+kubectl create -f ../clustered.ml/tensorflow-ps1-svc.yaml
+
+echo '...Worker0...'
+kubectl create -f ../clustered.ml/tensorflow-worker0-rc.yaml
+kubectl create -f ../clustered.ml/tensorflow-worker0-svc.yaml
+
+echo '...Worker1...'
+kubectl create -f ../clustered.ml/tensorflow-worker0-rc.yaml
+kubectl create -f ../clustered.ml/tensorflow-worker0-svc.yaml
