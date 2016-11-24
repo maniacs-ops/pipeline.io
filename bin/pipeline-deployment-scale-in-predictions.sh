0def778e38b67995e75921f6da43875e2f6ef910
cfregly <chris@fregly.com>
2016-11-24 18:00:47 +0000

    added codegen

--- bin/pipeline-deployment-scale-in-predictions.sh
+++ bin/pipeline-deployment-scale-in-predictions.sh
@@ -1,3 +1,4 @@
 #!/bin/sh                                                                               
                                                                                          
-kubectl scale --replicas=1 rc prediction-pmml 
+kubectl scale --replicas=1 rc prediction-pmml
+kubectl scale --replicas=1 rc prediction-codegen
--- bin/pipeline-deployment-scale-out-predictions.sh
+++ bin/pipeline-deployment-scale-out-predictions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh                                                                               
                                                                                         
-kubectl scale --replicas=5 rc prediction-pmml
-#kubectl scale --replicas=5 rc prediction-codegen
+kubectl scale --replicas=2 rc prediction-pmml
+kubectl scale --replicas=2 rc prediction-codegen
 #kubectl scale --replicas=5 rc prediction-cache 
 #kubectl scale --replicas=5 rc prediction-tensorflow                                           
